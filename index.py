3909d8fbd3cbfa6c0e5f0ed23ec3138e6a3802f5
Jorge Vasconez <jorgevasconez77@gmail.com>
2022-07-22 10:37:10 -0500

    'uno

--- index.py
+++ index.py
@@ -1,4 +1,16 @@
 from flask import Flask, render_template
+from flask import Flask,render_template
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+
+
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='Jorge_7789'
+app.config['MYSQL_DB']='atpuce_com'
+mysql=MySQL(app)
 
 app = Flask(__name__)
 
@@ -9,6 +21,13 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
+    usuario=request.form['usuario']
+    password=request.form['password']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
