60052b3205b08ad36b33c9a53ffd4a0f03875ae6
jorgevasconez77 <jorgevasconez77@gmail.com>
2023-03-23 07:51:43 -0500

    Version 3 transacciones

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import os
 from distutils.util import execute
 from mmap import PROT_WRITE
 
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify,json
 from flaskext.mysql import MySQL
 
 # Change the format of messages logged to Stackdriver
@@ -16,7 +16,6 @@ from flaskext.mysql import MySQL
    # "CACHE_DEFAULT_TIMEOUT": 300,
 #}
 userValidado = ""
-
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
 
@@ -64,7 +63,7 @@ def password():
 
 @app.route('/valida_inicio', methods=['POST'])
 def valida_inicio():
-
+    
     usuario = request.form['usuario']
     password = request.form['password']
 
@@ -94,7 +93,10 @@ def valida_inicio():
             global userValidado
             userValidado = usuario
             return redirect(url_for('saldos'))
-
+        
+@app.route('/encera_input', methods=['POST'])
+def encera_input():
+    texto=request.form['password']
 
 @app.route('/cambio_password', methods=['GET', 'POST'])
 def cambio_password():
@@ -176,8 +178,7 @@ def saldos():
 
     return render_template('saldos.html', captaciones=dataCp, colocaciones=dataCo, tCapital=totalCapital, tInteres=totalInteres, tTotal=total)
 
-# @app.route('/saldos')
-def prestamos():
+def prestamos(): 
     cur = mysql.get_db().cursor()
     cur.execute("""
         SELECT 
@@ -187,7 +188,8 @@ def prestamos():
         concat(co_pr_prestamo.Co_Pr_NoCuotas, '/' ,round((co_pr_prestamo.Co_Pr_ValSoli - co_pr_prestamo.Co_Pr_VSaldo)/(co_pr_prestamo.Co_Pr_ValSoli /co_pr_prestamo.Co_Pr_NoCuotas),0))
         as Cuotas,
         co_pr_prestamo.Co_Pr_FechaAd, 
-        concat(co_de_destino.Co_De_Descrip, ' ' , co_pr_prestamo.Co_Pr_Descrip) as Co_Pr_Descrip
+        concat(co_de_destino.Co_De_Descrip, ' ' , co_pr_prestamo.Co_Pr_Descrip) as Co_Pr_Descrip,
+        co_pr_prestamo.Co_Pr_Secuenci
 	    FROM
         cl_clientes
         INNER JOIN co_pr_prestamo on cl_clientes.Cl_Secuenci=co_pr_prestamo.Cl_Secuenci
@@ -197,9 +199,98 @@ def prestamos():
         AND cl_clientes.cl_numident='% s'  """ % userValidado)
 
     dataCo = cur.fetchall()
-    # print(dataCo)
     return dataCo
     # return render_template('saldos.html', colocaciones=dataCo)
+
+@app.route('/busca_prestamos')
+def busca_prestamos(): 
+    listaPrestamos=prestamos()
+    return render_template('prestamos.html',colocaciones=listaPrestamos)
+
+def prestamo_transaccion(secuenci):
+    print(secuenci)
+    cur = mysql.get_db().cursor()
+    cur.execute(
+        """ SELECT Co_Tr_Tipo,Co_Tr_CVigen,Co_Tr_IntNorma,Co_Tr_Efectivo+Co_Tr_Transfer as AbonoTotal,
+        Co_Tr_Fecha,Co_Tr_Saldo,Co_Tr_InDeven
+        FROM co_tr_transaccion WHERE Co_Pr_Secuenci='% s'
+        ORDER BY co_Tr_fecha ASC  """ % secuenci);
+    dataTr = cur.fetchall()
+    transacciones=dataTr
+    return(transacciones)
+
+@app.route('/prestamo_transaccion_secuenci<int:secuenci>,<string:nombreProducto>,<int:secuenciCesantiaPrestamo>')
+def prestamo_transaccion_secuenci(secuenci,nombreProducto,secuenciCesantiaPrestamo):
+    print(nombreProducto)
+    tr=prestamo_transaccion(secuenci)
+    #return(transacciones)
+    # Convertir los resultados a una cadena de texto JSON
+    transacciones_json = json.dumps(tr)
+    # Devolver la cadena de texto JSON
+    #return render_template('transaccionesAhorro.html', transacciones_json=transacciones_json)
+    return render_template('transaccionesPrestamo.html', transacciones=tr,nombrePR=nombreProducto,secuenciPrestamo=secuenci,secuenciPrestamoCesantia=secuenciCesantiaPrestamo)
+    #return (transacciones_json)
+
+@app.route('/ahorro')
+def ahorro():
+    cur = mysql.get_db().cursor()
+    cur.execute("""
+        SELECT 
+        cl_clientes.Cl_NumIdent, cl_clientes.Cl_Nombres, cp_pp_productopasivo.Cp_Pp_Secuenci, cp_pp_productopasivo.Cp_PP_NombrePP, 
+        cp_sl_saldo.Cp_Sl_Saldo,
+        cp_sl_saldo.Cp_Sl_Fecha, cp_sl_saldo.Cp_Sl_Secuenci
+        FROM 
+        cl_clientes 
+        INNER JOIN cp_sl_saldo on cl_clientes.Cl_Secuenci=cp_sl_saldo.Cl_Secuenci 
+        INNER JOIN cp_pp_productopasivo on cp_sl_saldo.Cp_Pp_Secuenci=cp_pp_productopasivo.Cp_Pp_Secuenci
+                WHERE cl_clientes.cl_estado='A' 
+        AND cl_clientes.cl_numident='% s'  """ % userValidado)
+
+    dataCp = cur.fetchall()
+
+    return render_template('ahorro.html', captaciones=dataCp)
+
+
+def ahorro_transaccion(secuenci):
+    cur = mysql.get_db().cursor()
+    cur.execute("""
+        SELECT cp_tr_transaccion.Cp_Tr_Tipo,cp_tr_transaccion.Cp_Tr_Efectivo, cp_tr_transaccion.Cp_Tr_Fecha,
+        cp_tr_transaccion.Cp_Tr_Saldo FROM cp_tr_transaccion
+        WHERE cp_tr_transaccion.Cp_Sl_Secuenci='% s' 
+        ORDER BY cp_tr_transaccion.Cp_Tr_Fecha DESC
+        LIMIT 15
+        """ % secuenci)
+    dataTr = cur.fetchall()
+    transacciones=dataTr
+    return(transacciones)
+
+@app.route('/ahorro_transaccion_secuenci<int:secuenci>,<string:nombreProducto>')
+def ahorro_transaccion_secuenci(secuenci,nombreProducto):
+    print(nombreProducto)
+    tr=ahorro_transaccion(secuenci)
+    #return(transacciones)
+    # Convertir los resultados a una cadena de texto JSON
+    transacciones_json = json.dumps(tr)
+    # Devolver la cadena de texto JSON
+    #return render_template('transaccionesAhorro.html', transacciones_json=transacciones_json)
+    return render_template('transaccionesAhorro.html', transacciones=tr,nombreCP=nombreProducto)
+    #return (transacciones_json)
+
+""" @app.route('/descuentos<int:secuenci>', methods=['GET', 'POST'])
+def descuentos(secuenci):
+    if request.method == 'POST':
+        selected_item = request.form.get('lista')
+        cur = mysql.get_db().cursor()
+        cur.execute(' SELECT * FROM atpuce_com.de_fd_fechadistribucion WHERE De_Fd_Fecha= %s;' (selected_item) )
+        dataItem = cur.fetchall()
+        item=dataItem
+        return render_template('grilla.html', items_grilla=item)
+    else:
+        cur = mysql.get_db().cursor()
+        cur.execute('SELECT * FROM atpuce_com.de_fd_fechadistribucion order by De_Fd_Fecha desc;')
+        items = cur.fetchall()
+        return render_template('descuentos.html', items=items) """
+
 
 
 if __name__ == '__main__':
